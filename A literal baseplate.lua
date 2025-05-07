9be9d75141f03d102b794e1c3584c1465a609727
DatUnknownGuy <test30581657@gmail.com>
2025-05-07 16:14:14 -0500

    A literal baseplate.lua

--- A literal baseplate.lua
+++ A literal baseplate.lua
@@ -17,8 +17,8 @@ local Tabs = {
     Main = Window:CreateTab{ Title = "Main", Icon = "layout-dashboard" },
     Movement = Window:CreateTab{ Title = "Movement", Icon = "hand" },
     Utilities = Window:CreateTab{ Title = "Utilities", Icon = "pen-line" },
-    Settings = Window:CreateTab{ Title = "Settings", Icon = "settings" },
-    Credits = Window:CreateTab{ Title = "Credits", Icon = "badge-info" }
+    Credits = Window:CreateTab{ Title = "Credits", Icon = "badge-info" },
+    Settings = Window:CreateTab{ Title = "Settings", Icon = "settings" }
 }
 
 local Options = Library.Options
